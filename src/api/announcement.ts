1065480bd1b188cd19d1639584cfbe566014c6d3
hermess1215 <165272998+hermess1215@users.noreply.github.com>
2025-11-29 21:48:06 +0900

    학생 완성

--- src/api/announcement.ts
+++ src/api/announcement.ts
@@ -1,6 +1,6 @@
 import { api } from "./client";
 
 export const getAnnouncements = async () => {
-  const res = await api.get("/notice/list"); // <-- 백엔드 엔드포인트에 맞게 수정
+  const res = await api.get("/api/students/me/notices"); // <-- 백엔드 엔드포인트에 맞게 수정
   return res.data;
 };
